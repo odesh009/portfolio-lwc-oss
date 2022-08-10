0ac98bcde15f40b9120e989f3ba6e50a3afb38a8
Om Deshmukh <odesh009@gmail.com>
2022-08-11 01:21:52 +0530

    delete deprecated

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { createElement } from 'lwc';
-import MyApp from 'my/app';
-
-const app = createElement('my-app', { is: MyApp });
-// eslint-disable-next-line @lwc/lwc/no-document-query
-console.log(document.querySelector('body'));
-document.querySelector('#main').appendChild(app);
